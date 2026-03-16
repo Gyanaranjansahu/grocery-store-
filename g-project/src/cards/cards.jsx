c98914f15e534175645a01e2fb353dc77b8b5ae0
gyana <gyana9619@gmail.com>
2026-03-16 10:45:28 +0530

    data

--- g-project/src/cards/cards.jsx
+++ g-project/src/cards/cards.jsx
@@ -1,24 +1,37 @@
+import { useState } from "react";
 import { FaHeart, FaPlus } from "react-icons/fa";
-
-export default function Cards({value}) {
-    return(
-       <div style={{gap:"1rem", display:"flex", alignItems:"center", justifyContent:"center", padding:"25px", position:"sticky", zIndex:"-1",
-        boxShadow:"1px 1px 4px rgba(0,0,0,0.1)", borderRadius:"5px", background:"wheat"
-        }  }>
-       <div className="se" style={{display:"flex",flexDirection:"column"}}>
-         <div style={{display:"flex", alignItems:"center", justifyContent:"space-between", gap:"4rem" }}>
-            <span><FaHeart/></span>
-            <span style={{ display:"flex", alignItems:"center",
-                justifyContent:"center", color:"white", background:"orangered", fontSize:"smaller", padding:"5px", borderRadius:"5px"
-            }}> <FaPlus/> </span>
+import "./card.css"
+export default function Cards({ value }) {
+    const [change, setChange] = useState(false)
+    return (
+        <div style={{
+            gap: "1rem", display: "flex", alignItems: "center", justifyContent: "center", padding: "25px",  zIndex: "0",
+            boxShadow: "1px 1px 4px rgba(0,0,0,0.1)", borderRadius: "5px", background: "wheat"
+        }}>
+            <div className="se"
+                style={{ display: "flex", flexDirection: "column" }}
+            >
+                <div style={{
+                    display: "flex", alignItems: "center", justifyContent: "space-between", gap: "4rem"
+                }}>
+                    <span style={{
+                         display: "flex", alignItems: "center", justifyContent: "center", padding: "4px", cursor: "pointer",
+                    }} onClick={() => setChange(!change)}    className={change ? "effect":""}><FaHeart /></span>
+                    <span style={{
+                        display: "flex", alignItems: "center", cursor: "pointer",
+                        justifyContent: "center", color: "white", background: "orangered", fontSize: "smaller", padding: "5px", borderRadius: "5px"
+                    }}> <FaPlus /> </span>
+                </div>
+                <div >
+                    <img src={value.Image} alt="" width="90" />
+                </div>
+                <span>{value.name}</span>
+                <span>{value.price}</span>
+                <button style={{
+                    border: "none", borderRadius: "4px", background: "orangered", padding: "3px 5px", color: "white ", display: "flex",
+                    alignItems: "center", justifyContent: "center", cursor:"pointer"
+                }}>Shop Now</button>
+            </div>
         </div>
-        <div >
-         <img src={value.Image} alt=""  width="90" />
-        </div>
-        <span>{value.name}</span>
-        <span>{value.price}</span>
-        <button style={{border:"none", borderRadius:"4px", background:"orangered", position:"absolute", bottom:"2px", left:"35px", padding:"3px 5px", color:"white "}}>Shop Now</button>
-       </div>
-       </div>
     )
 };
