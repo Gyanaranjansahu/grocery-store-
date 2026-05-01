784ee9bc69f1725a41746d9f538ff212d68cbe33
gyana <gyana9619@gmail.com>
2026-05-01 18:41:38 +0530

    add

--- g-project/src/product/product.jsx
+++ g-project/src/product/product.jsx
@@ -1,36 +1,36 @@
-import {  useState } from "react"
+import { useState } from "react"
 import lists from "../productlist/list"
 import "./product.css"
 import Cards from "../cards/cards"
 import Nav from "../navbar/nav"
-export default function  Product(){
-    let items=["All","Fruits","Vegetables","Dairy","SeaFood"]
-    const[active,setActive]=useState("All")
-    let categoryItem=active==="All"?lists:lists.filter(i=>i.category===active)
-    let choice=categoryItem.slice(0,8).map((ca , ids)=>{
-        return(
-            <Cards key={ids} value={ca}/>
+export default function Product() {
+    let items = ["All", "Fruits", "Vegetables", "Dairy", "SeaFood"]
+    const [active, setActive] = useState("All")
+    let categoryItem = active === "All" ? lists : lists.filter(i => i.category === active)
+    let choice = categoryItem.slice(0, 8).map((ca, ids) => {
+        return (
+            <Cards key={ids} value={ca} />
         )
     })
-    return(
-        <section className="fours" style={{display:"flex"}}>
-                <div className="p">
-                     <div style={{margin:"10px"}} > <h4 id="orange"> <span style={{ color: "orangered" }}>Our </span>Products</h4></div>
-                 <div className="product-list" > 
+    return (
+        <section className="fours" style={{ display: "flex" }}>
+            <div className="p">
+                <div style={{ margin: "10px" }} > <h4 id="orange"> <span style={{ color: "orangered" }}>Our </span>Products</h4></div>
+                <div className="product-list" >
                     {
-                        items.map((val,id)=>{
-                            return(
-                                <button id="b" key={id} className={active===val?"color":"white"} onClick={()=>setActive(val)}>
+                        items.map((val, id) => {
+                            return (
+                                <button id="b" key={id} className={active === val ? "color" : "white"} onClick={() => setActive(val)}>
                                     {val}
                                 </button>
                             )
                         })
                     }
-                 </div>
-                 <div className="card-section">
+                </div>
+                <div className="card-section">
                     {choice}
                  </div>
-                </div>
+            </div>
         </section>
     )
 }
